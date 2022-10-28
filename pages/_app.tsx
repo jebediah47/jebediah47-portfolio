0da354a0214f7fafca7b8622f7aab216e2c383a8
Christian Llupo <jebediah47@protonmail.com>
2022-10-28 20:00:08 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,4 @@
-import "../styles/projects_background_blob.css";
-import "../styles/skills_background_blob.css";
-import "../styles/projects_animation.css";
 import type { AppProps } from "next/app";
-import "../styles/skills_animation.css";
-import "../styles/hero_animation.css";
-import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
